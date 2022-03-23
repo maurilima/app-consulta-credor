60920b6d3a4a738f27bda140024e46878b24c666
maurilima <mauri.lima.roraima@gmail.com>
2022-03-23 13:29:22 -0400

    Atauliza

--- src/components/ViewEmpenho/index.jsx
+++ src/components/ViewEmpenho/index.jsx
@@ -1,66 +1,51 @@
 import { useEffect, useState } from "react";
-import { Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import DataTable from "react-data-table-component";
 import { GetApiEndPoint } from "../../Api/getApiEndPoint";
 import { URL_DC } from "../../Config/urlApi";
 import { columnsEmpenho, paginationOptions } from "../PaginationTable/options";
+import { ProgressCircle } from "../../utils/Progress";
+import { clearData } from "../../Config/Util/libUtil";
 
 let numEmpenho = "";
 
-// require('react-dom');
-// window.React2 = require('react');
-// console.log(window.React1 === window.React2);
 export const ViewEmpenho = ({ lRow }) => {
+  let cabecalho = {}  ;
   const [empenho, setEmpenho] = useState([]);
   const [loading, setLoading] = useState(false);
+//   const [cabecalho,setCabecalho] = useState({})
 
-  console.log(lRow);
   numEmpenho = lRow;
 
   const showData = async (raw) => {
-    // console.log(raw);
+    console.log(raw);
     setLoading(true);
     raw = { numeroEmpenho: raw };
     raw = { ...raw, page: 0, size: 200 };
-    console.log(raw);
     const response = await GetApiEndPoint(raw, URL_DC);
-    // console.log(response.con);
     setEmpenho(response.content[0].despesaPorCredorExecucao);
+    cabecalho = clearData(response.content);
+    console.log(cabecalho)
+
+    console.log(cabecalho.numeroEmpenho)
     setLoading(false);
   };
 
   useEffect(() => {
-    // console.log(numEmpenho);
-
+       /* eslint-disable */
     showData(numEmpenho);
   }, []);
 
-  //   console.log(empenho)
 
-  //   if (lRow == null ) {
-  //     console.log("sim");
-
-  //     console.log(numEMpenho);
-  //   } else {
-  //     numEMpenho = lRow;
-  //     console.log("Nao");
-  //     console.log(lRow);
-  //   }
-
-  //   //   if (Object.values(lRow).length > 0) {
-  //   if (numEMpenho !== null) {
-  //     //   numEMpenho = lRow;
-  //     console.log(numEMpenho);
-
-  //     // const retorno = showData(numEMpenho);
-  //     numEMpenho = "";
-  //     console.log(numEMpenho);
-  //   }
-  //   //   }
 
   return (
     <>
-      <Row className="empenho"></Row>
+      <Row className="empenho">
+          <Col>
+              <h5>Nº Empenho:</h5>
+              <h4>{cabecalho.numeroEmpenho}</h4>
+          </Col>
+      </Row>
       <div className="tabela">
             <DataTable
               title="Pagamentos"
@@ -70,13 +55,8 @@ export const ViewEmpenho = ({ lRow }) => {
               true
               progressPending={loading}
               pagination
-              // paginationServer
-              // paginationTotalRows={totalRows}
-              // paginationDefaultPage={currentPage}
-              // onChangeRowsPerPage={handlePerRowsChange}
-              // onChangePage={handlePageChange}
               paginationComponentOptions={paginationOptions}
-              // progressComponent={<ProgressCircle />}
+              progressComponent={<ProgressCircle />}
             />
           </div>
     </>
